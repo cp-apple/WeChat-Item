e825a56b3410ed69db049de4e1be733a22601ca6
cp-apple <eryaping@qq.com>
2017-09-07 18:04:47 +0800

    你问我答语音聊天

--- pages/myVoice/youQiA/youQiA.js
+++ pages/myVoice/youQiA/youQiA.js
@@ -1,5 +1,5 @@
 // 你问我答
-var app= getApp()
+var app = getApp()
 Page({
 
   /**
@@ -7,8 +7,9 @@ Page({
    */
   data: {
     saySomething: [],
-    iptValue:'',
-    focus: false
+    iptValue: '',
+    focus: false,
+    microphone:1  //麦克风，1灰，2绿
   },
 
   /**
@@ -24,16 +25,63 @@ Page({
   /**
    * 发送消息,提交表单
    */
-  formSubmit:function(e){
+  formSubmit: function (e) {
     var sayValue = e.detail.value.sayValue;
-    if(sayValue!=''){
+    if (sayValue != '') {
       this.data.saySomething.push(e.detail.value.sayValue);
       this.setData({
         saySomething: this.data.saySomething,
         iptValue: '',
-        focus:true
+        focus: true
       });
     }
-    
+
+  },
+  /**
+   * 开始录音
+   */
+  startRecord: function (e) {
+    var _that=this;
+    _that.setData({
+      microphone:2
+    });
+   
+    wx.startRecord({
+      success: function (res) {
+        var tempFilePath = res.tempFilePath;
+        _that.data.saySomething.push(tempFilePath);
+        _that.setData({
+          saySomething: _that.data.saySomething
+        });
+        
+        wx.showToast({
+          title: '发送成功，音频文件路径为：' + tempFilePath,
+        });
+      },
+      fail: function (res) {
+        //录音失败
+        wx.showToast({
+          title: '录音失败。',
+        });        
+      },
+      complete:function(){
+        _that.setData({
+          microphone: 1
+        });
+      }
+    });   
+
+    setTimeout(function () {
+      //结束录音  
+      wx.stopRecord();      
+    }, 10000)
+  },
+
+  /**
+    * 结束录音
+    */
+  endRecord: function (e) {
+    wx.stopRecord();   
   }
+
 })
